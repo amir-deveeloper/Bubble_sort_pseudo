d147cd2b76e879017cae2aabc4383076ffb9575b
amir <khamidullin.amir@gmail.com>
2022-02-13 20:44:32 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,59 +7,83 @@ import static java.util.Arrays.copyOfRange;
 
 public class Main {
 
+
     public static void main(String[] args) {
+        int[] num = {1,2,3,4,5};
+        int k = 0;
+        int s = Math.abs(k - num[0]);
+        int ans = num[0];
 
-        Scanner scanner = new Scanner(System.in);
 
-        String[] str = scanner.nextLine().split(" ");
-        String[] str1 = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(str[0]);
-        int d = Integer.parseInt(str[1]);
-
-        int[] num = new int[n];
-        int[] rev = new int[d];
-        int j = 0;
-
+        int n = num.length;
+        int counter = 0;
         for (int i = 0; i < n; i++){
+            int a;
+            a = Math.abs(k - num[i]);
+            if (a < s){
+                s = a;
+                ans = num[i];
+            }
+        }
+        System.out.println(ans);
 
-            num[i] = Integer.parseInt(str1[i]);
+    }
+
+
+
+
+
+
+        public static void bubbleSort(int[] arr){
+
+        /*
+
+            int n = arr.length;
+            int temp;
+            int counter = 0;
+            for (int i = 0; i < n-1; i++){
+                for (int j = 0; j < n-1; j++){
+                    counter++;
+                    if (arr[j] > arr[j+1]){
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+
+                }
+            }
+            System.out.println(counter);
+            System.out.println(Arrays.toString(arr));
+
+         */
+/*
+            int n = arr.length;
+            int temp;
+            for (int i = 0; i < n-1; i++){
+                int min = i;
+                for (int j = i+1; j < n; j++){
+
+                    if (arr[j] < arr[min]){
+                        min = j;
+                    }
+                }
+
+                temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+
+
+
+            }
+            System.out.println(Arrays.toString(arr));
+
+ */
+
+
 
 
         }
-
-        for (int i = 0; i < d; i++){
-
-            rev[i] = num[i];
-
-
-        }
-
-
-        for (int i = 0; i < n - d; i++){
-
-            int k = num[i];
-
-            num[i] = num[d+i];
-
-
-        }
-
-        for (int i = n - d; i < n; i++){
-
-            num[i] = rev[j];
-            j++;
-        }
-
-
-
-
-
-
-        System.out.println(java.util.Arrays.toString(num));
-
-
 
 
     }
-}
+
